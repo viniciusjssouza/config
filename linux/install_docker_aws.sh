4e28bda0a538ab5b342b2a07890dc503d3e2debf
Vinícius Souza <viniciusjssouza@gmail.com>
2018-03-15 11:48:21 -0300

    Add docker install script
    credits for tony.lampada

---
+++ linux/install_docker_aws.sh
@@ -0,0 +1,23 @@
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+sudo apt-get install docker-ce
+
+sudo docker run hello-world
+sudo usermod -aG docker ubuntu
+
+COMPOSER_VERSION=1.19.0  # ver https://github.com/docker/compose/releases
+sudo curl -L https://github.com/docker/compose/releases/download/$COMPOSER_VERSION/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
